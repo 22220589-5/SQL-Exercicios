ef4e0572b285d3c0f6ece80168195e4ed528b1f6
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-25 16:47:55 -0300

    Update 1.7 - Consulta 5.sql

--- Exercícios Alura - Módulo 1/1.7 - Consulta 5.sql
+++ Exercícios Alura - Módulo 1/1.7 - Consulta 5.sql
@@ -2,7 +2,8 @@
 
 /*Mostrando o nome dos alunos, suas respectivas notas e o status da aprovação: */
 
-select alunos.nome_aluno, notas.valor_nota,
+select 
+	alunos.nome_aluno, notas.valor_nota,
 case 
 	when notas.valor_nota >= 6 then 'Aprovado'
     else 'Reprovado'
@@ -13,7 +14,8 @@ on notas.id_aluno_fk = alunos.id_aluno;
 
 /*Caso haja a necessidade de saber quantidade de alunos aprovados (21) e reprovados (29) segundo o critério do enunciado:*/
 
-select count(*) as Quantidade,
+select 
+	count(*) as Quantidade,
 case 
 	when valor_nota >= 6 then 'Aprovado'
     else 'Reprovado'
