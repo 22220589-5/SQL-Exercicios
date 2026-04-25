94bc134da82a98fecbfca1378262d7e2736eff8d
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-25 16:46:51 -0300

    Update 1.5 - Consulta 3.sql

--- Exercícios Alura - Módulo 1/1.5 - Consulta 3.sql
+++ Exercícios Alura - Módulo 1/1.5 - Consulta 3.sql
@@ -1,7 +1,12 @@
-/*Consulta 3: Buscar apenas os alunos que fazem aniversário em fevereiro.*/
+-- Consulta 3: Buscar apenas os alunos que fazem aniversário em fevereiro
 
+-- Forma 1:
 select nome_aluno from alunos
-where data_nascimento_aluno like '____-02-__'; /*o sublinhado no operador like força a necessidade de existir um caractere*/
+where data_nascimento_aluno like '____-02-__'; -- o sublinhado no operador like força a necessidade de existir um caractere
+
+--Forma 2:
+select nome_aluno from alunos
+where month(data_nasciment_aluno) = '02'; -- year(), month(), day() usamos no mysql
 
 /*Resultado:
 Carolina Oliveira
