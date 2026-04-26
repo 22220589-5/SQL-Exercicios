08ef694590e8e8f4236addcf1b8108a7a8159027
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:20:08 -0300

    Delete Exercícios Alura - Módulo 2/2 - Consultas/2.9 Consulta 9.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/2.9 Consulta 9.sql
+++
@@ -1,7 +0,0 @@
--- Consulta 9: Liste os produtos e o ID do pedido que foram realizados pelo cliente "Pedro Alves" ou pela cliente "Ana Rodrigues".
-
-select produtos.nome, pedidos.id_pedido, clientes.nome 
-from pedidos join itens_de_pedido on pedidos.id_pedido = itens_de_pedido.id_pedido_fk
-join clientes on clientes.id_cliente = pedidos.id_cliente_fk
-join produtos on itens_de_pedido.id_produto_fk = produtos.id_produto
-where clientes.nome = 'Pedro Alves' or clientes.nome = 'Ana Rodrigues';
