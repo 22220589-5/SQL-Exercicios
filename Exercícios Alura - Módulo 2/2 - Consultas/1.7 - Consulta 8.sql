b8727cc663e1c03b171822de7748114861b67eb2
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:41:36 -0300

    Update 1.7 - Consulta 8.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/1.7 - Consulta 8.sql
+++ Exercícios Alura - Módulo 2/2 - Consultas/1.7 - Consulta 8.sql
@@ -1,10 +1,15 @@
 -- Consulta 8: Recupere os nomes dos produtos que estão em menos de 20 pedidos
 
-select produtos.id_produto, produtos.nome, count(*) as qtde_pedidos
-from itens_de_pedido join produtos on itens_de_pedido.id_produto_fk = produtos.id_produto
-group by id_produto_fk
-having qtde_pedidos < 20
-order by qtde_pedidos desc;
+select 
+  produtos.id_produto, produtos.nome, count(*) as qtde_pedidos
+from 
+  itens_de_pedido join produtos on itens_de_pedido.id_produto_fk = produtos.id_produto
+group by 
+  id_produto_fk
+having 
+  qtde_pedidos < 20
+order by 
+  qtde_pedidos desc;
 
 /*
 Resultado: 
