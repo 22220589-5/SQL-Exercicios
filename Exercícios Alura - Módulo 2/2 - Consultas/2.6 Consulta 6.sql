83859094b0491299f33e6872696d49a963ca8ef9
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:18:24 -0300

    Delete Exercícios Alura - Módulo 2/2 - Consultas/2.6 Consulta 6.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/2.6 Consulta 6.sql
+++
@@ -1,9 +0,0 @@
--- Consulta 6: Liste os produtos que nunca foram pedidos
-
-select produtos.id_produto, produtos.nome 
-from itens_de_pedido right join produtos on itens_de_pedido.id_produto_fk = produtos.id_produto
-where itens_de_pedido.id_pedido_fk is null;
-
-/*
-Resultado: Arroz Carreteiro (id = 12)
-*/
