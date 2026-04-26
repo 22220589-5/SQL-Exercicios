2e3d85048ff81a1469e3c5abcf82aa47a0e56a25
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:43:03 -0300

    Update 1.1 - Consulta 2.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/1.1 - Consulta 2.sql
+++ Exercícios Alura - Módulo 2/2 - Consultas/1.1 - Consulta 2.sql
@@ -1,3 +1,8 @@
 -- Consulta 2: Busque o ID do pedido e o ID do cliente dos pedidos onde o status esteja como entregue
 
-select id_pedido, id_cliente_fk, status_pedido from pedidos where status_pedido = 'Entregue';
+select 
+  id_pedido, id_cliente_fk, status_pedido 
+from 
+  pedidos 
+where 
+  status_pedido = 'Entregue';
