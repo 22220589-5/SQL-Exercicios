6ba0d50e8a867099956b065caaea56198b85bd32
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:38:29 -0300

    Create 2 Consulta 11.sql

---
+++ Exercícios Alura - Módulo 2/2 - Consultas/2 Consulta 11.sql
@@ -0,0 +1,9 @@
+-- Consulta 11 (extra): mostrar a quantidade total que foi pedida de cada produto em ordem decrescente
+
+select produtos.id_produto, produtos.nome, sum(itens_de_pedido.quantidade) as quantidade_total
+from itens_de_pedido right join produtos on itens_de_pedido.id_produto_fk = produtos.id_produto
+group by itens_de_pedido.id_produto_fk
+order by quantidade_total desc;
+
+-- Tirando a prova real:
+select sum(quantidade) from itens_de_pedido where id_produto_fk = 2;
