bddde0227e7ecfa5ca224942c1b017e447dd4b33
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 14:32:13 -0300

    Create 2.4 Consulta 4.sql

---
+++ Exercícios Alura - Módulo 2/2 - Consultas/2.4 Consulta 4.sql
@@ -0,0 +1,8 @@
+-- Consulta 4: Busque o nome e cargo dos colaboradores que foram contratados entre 2022-01-01 e 2022-06-30
+
+select id_colaborador, nome, cargo, data_contratacao from colaboradores
+where data_contratacao > '2022-01-01' and data_contratacao < '2022-06-30';
+
+/*
+Resultado: 4 colaboradores de id 1, 2, 4 e 6
+*/
