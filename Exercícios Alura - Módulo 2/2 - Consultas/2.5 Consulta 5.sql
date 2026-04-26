377d6863c25eddfbcb7008a95ee9fc9274efc590
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:17:51 -0300

    Delete Exercícios Alura - Módulo 2/2 - Consultas/2.5 Consulta 5.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/2.5 Consulta 5.sql
+++
@@ -1,9 +0,0 @@
--- Consulta 5: Recupere o nome do cliente que fez o primeiro pedido
-
-select clientes.id_cliente, clientes.nome 
-from pedidos join clientes on pedidos.id_cliente_fk = clientes.id_cliente
-where pedidos.id_pedido = 1;
-
-/*
-Resultado: Ana Maria Silva (id = 10)
-*/
