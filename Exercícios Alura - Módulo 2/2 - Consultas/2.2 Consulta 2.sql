54f953b82e8688252d2ededb9f2abf57a6c1f5a0
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 20:16:01 -0300

    Delete Exercícios Alura - Módulo 2/2 - Consultas/2.2 Consulta 2.sql

--- Exercícios Alura - Módulo 2/2 - Consultas/2.2 Consulta 2.sql
+++
@@ -1,3 +0,0 @@
--- Consulta 2: Busque o ID do pedido e o ID do cliente dos pedidos onde o status esteja como entregue
-
-select id_pedido, id_cliente_fk, status_pedido from pedidos where status_pedido = 'Entregue';
