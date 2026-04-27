fb18c745d933454b79b7ecf38a5e9ee9c599a113
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-26 22:07:48 -0300

    Create 2.4 Consulta 4.sql

---
+++ Exercícios Alura - Módulo 1/2.4 Consulta 4.sql
@@ -0,0 +1,11 @@
+-- Consulta 4: Listar os Alunos e as disciplinas em que estão matriculados
+
+-- Lista os alunos ordenando pelo nome deles e as disciplinas cursadas
+select 
+	alunos.nome_aluno, disciplinas.nome_disciplina
+from
+	turma_alunos join alunos on turma_alunos.id_aluno_fk = alunos.id_aluno
+    join turma_disciplinas on turma_alunos.id_turma_fk = turma_disciplinas.id_turma_fk
+    join disciplinas on turma_disciplinas.id_disciplina_fk = disciplinas.id_disciplina
+order by
+	alunos.nome_aluno;
