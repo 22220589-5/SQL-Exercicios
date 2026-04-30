10ff435f15c128ffe1f74a8df6e20e91f7cbd809
22220589-5 <153026030+22220589-5@users.noreply.github.com>
2026-04-29 22:01:44 -0300

    Update 5.3 - Consulta 4.sql

--- Exercícios Alura - Módulo 3/5.3 - Consulta 4.sql
+++ Exercícios Alura - Módulo 3/5.3 - Consulta 4.sql
@@ -1,11 +1,8 @@
 -- Consulta 04 - Qual o primeiro ano disponível na base?
 
 select
-	year(data_venda) as Ano
+	min(year(data_venda)) as Primeiro_Ano
 from 
-	vendas
-order by
-	data_venda
-LIMIT 1;
+	vendas;
     
 -- Resultado: Ano de 2020
